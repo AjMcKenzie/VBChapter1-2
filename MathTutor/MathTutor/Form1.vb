9b03db02c44140a7c2de8021a601bf6b220bbc05
Andrew <heldiggris7@gmail.com>
2017-11-01 10:32:59 -0500

    update MathTtutor

---
+++ MathTutor/MathTutor/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub btnShowAnwser_Click(sender As Object, e As EventArgs) Handles btnShowAnwser.Click
+        lblAnwser.Text = "18 + 64 = 82"
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
