0566cb47aac0b49551c0ff8461e7e67dab3c56f4
Andrew <heldiggris7@gmail.com>
2017-11-02 10:40:43 -0500

    update JokeAndPunchLine

---
+++ JokeAndPunchLine/JokeAndPunchLine/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSetup = New System.Windows.Forms.Button()
+        Me.btnPunchLine = New System.Windows.Forms.Button()
+        Me.lblJoke = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSetup
+        '
+        Me.btnSetup.Location = New System.Drawing.Point(49, 134)
+        Me.btnSetup.Name = "btnSetup"
+        Me.btnSetup.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetup.TabIndex = 0
+        Me.btnSetup.Text = "Setup"
+        Me.btnSetup.UseVisualStyleBackColor = True
+        '
+        'btnPunchLine
+        '
+        Me.btnPunchLine.Location = New System.Drawing.Point(183, 134)
+        Me.btnPunchLine.Name = "btnPunchLine"
+        Me.btnPunchLine.Size = New System.Drawing.Size(75, 23)
+        Me.btnPunchLine.TabIndex = 1
+        Me.btnPunchLine.Text = "Punch Line"
+        Me.btnPunchLine.UseVisualStyleBackColor = True
+        '
+        'lblJoke
+        '
+        Me.lblJoke.AutoSize = True
+        Me.lblJoke.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJoke.Location = New System.Drawing.Point(12, 9)
+        Me.lblJoke.Name = "lblJoke"
+        Me.lblJoke.Size = New System.Drawing.Size(214, 27)
+        Me.lblJoke.TabIndex = 2
+        Me.lblJoke.Text = "Click buttons for joke"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(317, 169)
+        Me.Controls.Add(Me.lblJoke)
+        Me.Controls.Add(Me.btnPunchLine)
+        Me.Controls.Add(Me.btnSetup)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSetup As System.Windows.Forms.Button
+    Friend WithEvents btnPunchLine As System.Windows.Forms.Button
+    Friend WithEvents lblJoke As System.Windows.Forms.Label
+
+End Class
---
+++ JokeAndPunchLine/JokeAndPunchLine/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub btnSetup_Click(sender As Object, e As EventArgs) Handles btnSetup.Click
+        lblJoke.Text = "How many programmers does" & ControlChars.NewLine &
+            "it take to change a light bulb?"
+    End Sub
+
+    Private Sub btnPunchLine_Click(sender As Object, e As EventArgs) Handles btnPunchLine.Click
+        lblJoke.Text = "None." & ControlChars.NewLine &
+            "That's a hardware problem."
+    End Sub
+End Class
