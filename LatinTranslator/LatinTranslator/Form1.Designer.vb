e0c162b3b9b4d8e3932b8a6d788e6b109b422165
Andrew <heldiggris7@gmail.com>
2017-11-02 10:31:06 -0500

    update LatinTranslator finished

--- LatinTranslator/LatinTranslator/Form1.Designer.vb
+++ LatinTranslator/LatinTranslator/Form1.Designer.vb
@@ -22,30 +22,79 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnSinister = New System.Windows.Forms.Button()
+        Me.btnDexter = New System.Windows.Forms.Button()
+        Me.btnMedium = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'btnSinister
+        '
+        Me.btnSinister.Location = New System.Drawing.Point(97, 128)
+        Me.btnSinister.Name = "btnSinister"
+        Me.btnSinister.Size = New System.Drawing.Size(75, 23)
+        Me.btnSinister.TabIndex = 1
+        Me.btnSinister.Text = "Sinister"
+        Me.btnSinister.UseVisualStyleBackColor = True
+        '
+        'btnDexter
+        '
+        Me.btnDexter.Location = New System.Drawing.Point(212, 128)
+        Me.btnDexter.Name = "btnDexter"
+        Me.btnDexter.Size = New System.Drawing.Size(75, 23)
+        Me.btnDexter.TabIndex = 2
+        Me.btnDexter.Text = "Dexter"
+        Me.btnDexter.UseVisualStyleBackColor = True
+        '
+        'btnMedium
+        '
+        Me.btnMedium.Location = New System.Drawing.Point(326, 128)
+        Me.btnMedium.Name = "btnMedium"
+        Me.btnMedium.Size = New System.Drawing.Size(75, 23)
+        Me.btnMedium.TabIndex = 3
+        Me.btnMedium.Text = "Medium"
+        Me.btnMedium.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(93, 57)
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 54)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(446, 23)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Click buttons to translate these Latin words to English."
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(212, 172)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(465, 209)
+        Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnMedium)
+        Me.Controls.Add(Me.btnDexter)
+        Me.Controls.Add(Me.btnSinister)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btnSinister As System.Windows.Forms.Button
+    Friend WithEvents btnDexter As System.Windows.Forms.Button
+    Friend WithEvents btnMedium As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
 
 End Class
