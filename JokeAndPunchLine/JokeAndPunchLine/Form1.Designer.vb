1045eb02638cc28cd101d5021918a333c58cb683
Andrew <heldiggris7@gmail.com>
2017-11-02 11:13:39 -0500

    update JokeAndPunchLine
    Added a second joke

--- JokeAndPunchLine/JokeAndPunchLine/Form1.Designer.vb
+++ JokeAndPunchLine/JokeAndPunchLine/Form1.Designer.vb
@@ -25,11 +25,15 @@ Partial Class Form1
         Me.btnSetup = New System.Windows.Forms.Button()
         Me.btnPunchLine = New System.Windows.Forms.Button()
         Me.lblJoke = New System.Windows.Forms.Label()
+        Me.btnNextJoke = New System.Windows.Forms.Button()
+        Me.btnSteup2 = New System.Windows.Forms.Button()
+        Me.btnPunchLine2 = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnSetup
         '
-        Me.btnSetup.Location = New System.Drawing.Point(49, 134)
+        Me.btnSetup.Location = New System.Drawing.Point(17, 109)
         Me.btnSetup.Name = "btnSetup"
         Me.btnSetup.Size = New System.Drawing.Size(75, 23)
         Me.btnSetup.TabIndex = 0
@@ -38,7 +42,7 @@ Partial Class Form1
         '
         'btnPunchLine
         '
-        Me.btnPunchLine.Location = New System.Drawing.Point(183, 134)
+        Me.btnPunchLine.Location = New System.Drawing.Point(122, 109)
         Me.btnPunchLine.Name = "btnPunchLine"
         Me.btnPunchLine.Size = New System.Drawing.Size(75, 23)
         Me.btnPunchLine.TabIndex = 1
@@ -51,15 +55,58 @@ Partial Class Form1
         Me.lblJoke.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblJoke.Location = New System.Drawing.Point(12, 9)
         Me.lblJoke.Name = "lblJoke"
-        Me.lblJoke.Size = New System.Drawing.Size(214, 27)
+        Me.lblJoke.Size = New System.Drawing.Size(203, 27)
         Me.lblJoke.TabIndex = 2
-        Me.lblJoke.Text = "Click buttons for joke"
+        Me.lblJoke.Text = "Click setup for joke."
+        '
+        'btnNextJoke
+        '
+        Me.btnNextJoke.Location = New System.Drawing.Point(232, 109)
+        Me.btnNextJoke.Name = "btnNextJoke"
+        Me.btnNextJoke.Size = New System.Drawing.Size(75, 23)
+        Me.btnNextJoke.TabIndex = 3
+        Me.btnNextJoke.Text = "Next Joke"
+        Me.btnNextJoke.UseVisualStyleBackColor = True
+        '
+        'btnSteup2
+        '
+        Me.btnSteup2.Location = New System.Drawing.Point(17, 109)
+        Me.btnSteup2.Name = "btnSteup2"
+        Me.btnSteup2.Size = New System.Drawing.Size(75, 23)
+        Me.btnSteup2.TabIndex = 4
+        Me.btnSteup2.Text = "Setup"
+        Me.btnSteup2.UseVisualStyleBackColor = True
+        Me.btnSteup2.Visible = False
+        '
+        'btnPunchLine2
+        '
+        Me.btnPunchLine2.Location = New System.Drawing.Point(122, 109)
+        Me.btnPunchLine2.Name = "btnPunchLine2"
+        Me.btnPunchLine2.Size = New System.Drawing.Size(75, 23)
+        Me.btnPunchLine2.TabIndex = 5
+        Me.btnPunchLine2.Text = "Punch Line"
+        Me.btnPunchLine2.UseVisualStyleBackColor = True
+        Me.btnPunchLine2.Visible = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(232, 109)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        Me.btnExit.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(317, 169)
+        Me.ClientSize = New System.Drawing.Size(352, 181)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPunchLine2)
+        Me.Controls.Add(Me.btnSteup2)
+        Me.Controls.Add(Me.btnNextJoke)
         Me.Controls.Add(Me.lblJoke)
         Me.Controls.Add(Me.btnPunchLine)
         Me.Controls.Add(Me.btnSetup)
@@ -72,5 +119,9 @@ Partial Class Form1
     Friend WithEvents btnSetup As System.Windows.Forms.Button
     Friend WithEvents btnPunchLine As System.Windows.Forms.Button
     Friend WithEvents lblJoke As System.Windows.Forms.Label
+    Friend WithEvents btnNextJoke As System.Windows.Forms.Button
+    Friend WithEvents btnSteup2 As System.Windows.Forms.Button
+    Friend WithEvents btnPunchLine2 As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
 
 End Class
--- JokeAndPunchLine/JokeAndPunchLine/Form1.vb
+++ JokeAndPunchLine/JokeAndPunchLine/Form1.vb
@@ -9,4 +9,24 @@
         lblJoke.Text = "None." & ControlChars.NewLine &
             "That's a hardware problem."
     End Sub
+
+    Private Sub btnNextJoke_Click(sender As Object, e As EventArgs) Handles btnNextJoke.Click
+        btnSteup2.Show()
+        btnPunchLine2.Show()
+        btnExit.Show()
+        lblJoke.Text = "Click setup for new joke."
+    End Sub
+
+    Private Sub btnSteup2_Click(sender As Object, e As EventArgs) Handles btnSteup2.Click
+        lblJoke.Text = "Why do Java Programmers" & ControlChars.NewLine &
+            "have to wear glasses?"
+    End Sub
+
+    Private Sub btnPunchLine2_Click(sender As Object, e As EventArgs) Handles btnPunchLine2.Click
+        lblJoke.Text = "Because the don't C#"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
